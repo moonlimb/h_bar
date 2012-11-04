4a2e360a1a34e13af352386b7fd19ef32f12cbb2
moonlimb <moonlimb@gmail.com>
2012-11-03 21:29:15 -0700

    first commit

---
+++ drink.py
@@ -0,0 +1,14 @@
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship, backref
+import os
+
+class Cocktail():
+#	def __init__(self):
+	pass
+
+
---
+++ letsdrink.py
@@ -0,0 +1,29 @@
+from flask import Flask, session, render_template, redirect, request, url_for, g, flash, abort 
+import os
+import sqlite3
+
+# flask session: browser session (info identifying particular user of a web app)
+# model session: database session (connection to database)
+
+app = Flask (__name__)
+SECRET_KEY = "particle_physics_mixology"
+app.config.from_object(__name__)    
+
+# @app.teardown_request
+# def shutdown_session(exception = None):
+#     db_session.remove()
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/recipe",methods=['POST'])
+def order():
+	particle_config = request.form['particle_config']
+	
+	
+	return render_template("recipe.html", code = particle_config)
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
